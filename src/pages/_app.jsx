4d938c936b238776b7dd94ea32d0101e4a30ff0a
danielVFS <danielv759153@gmail.com>
2021-03-29 09:44:52 -0300

    configuring: styled components

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components"
+
+import { theme } from '../styles/theme';
+import { GlobalStyles } from '../styles/global-styles';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
