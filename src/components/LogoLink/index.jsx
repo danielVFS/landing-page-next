32c05e70e1a38c62feb378eca5d07a26f6695e9b
danielVFS <danielv759153@gmail.com>
2021-03-30 10:52:36 -0300

    Next/link

--- src/components/LogoLink/index.jsx
+++ src/components/LogoLink/index.jsx
@@ -1,9 +1,25 @@
 import P from 'prop-types';
+import Link from 'next/link';
 
 import * as Styled from './styles';
 import { Heading } from '../Heading';
 
 export const LogoLink = ({ text, srcImg = '', link }) => {
+  const nextLink = link.match(/^\//) ? true : false;
+
+  if (nextLink) {
+    return (
+      <Heading size="small" uppercase>
+        <Link href={link} passHref>
+          <Styled.Container>
+            {!!srcImg && <img src={srcImg} alt={text} />}
+            {!srcImg && text}
+          </Styled.Container>
+        </Link>
+      </Heading>
+    );
+  }
+
   return (
     <Heading size="small" uppercase>
       <Styled.Container href={link}>
