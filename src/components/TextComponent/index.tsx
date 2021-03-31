64a63caf8f4bc9ca26f185b37fe9610bd33d0ce5
danielVFS <danielv759153@gmail.com>
2021-03-31 15:53:23 -0300

    passing to TS: TextComponent

---
+++ src/components/TextComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styled from './styles';
+
+export type TextComponentProps = {
+  children: string;
+};
+
+export const TextComponent = ({ children }: TextComponentProps) => {
+  return <Styled.Container dangerouslySetInnerHTML={{ __html: children }} />;
+};
